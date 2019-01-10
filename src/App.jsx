7d3d2c41610658a25984ba010e48329c1b779dce
victor obije <konigeobije@gmail.com>
2019-01-10 09:27:46 +0100

    [chore 162170971] updated files from PR comments

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/routes';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes />
+      </Router>
+    );
+  }
+}
+
+export default App;
