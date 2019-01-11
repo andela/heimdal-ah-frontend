c8b780b468753a5a11c211fef2b754ddabccdf22
pitaz <peter.odekwo@gmail.com>
2019-01-11 11:06:01 +0100

    fix hound errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import Routes from './routes/routes';
 
