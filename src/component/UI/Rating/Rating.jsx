4532c5adc5d1a6bf3249e147b2d8e27a4db63b3f
pitaz <peter.odekwo@gmail.com>
2019-01-14 10:31:22 +0100

    [feature #163166708] create rating component

--- src/component/UI/Rating/Rating.jsx
+++ src/component/UI/Rating/Rating.jsx
@@ -6,50 +6,31 @@ export default class StarRating extends Component {
     minRating: 0,
     maxRating: 10,
     rating: 5,
-    starRatio: 2,
+    ratio: 2,
   }
 
   maxStars() {
-    const { maxRating, starRatio } = this.props;
-    return Math.ceil(maxRating / starRatio);
+    const { maxRating, ratio } = this.props;
+    return Math.ceil(maxRating / ratio);
   }
 
   fullStars() {
-    const { rating, starRatio } = this.props;
-    return Math.floor(rating / starRatio);
-  }
-
-  halfStars() {
-    const { rating, starRatio } = this.props;
-    const x = rating % starRatio;
-    const i = (1 / 2) * starRatio;
-    return x >= i ? 1 : 0;
+    const { rating, ratio } = this.props;
+    return Math.floor(rating / ratio);
   }
 
   emptyStars() {
-    return this.maxStars() - this.fullStars() - this.halfStars();
+    return this.maxStars() - this.fullStars();
   }
 
   render() {
     const fullStars = this.fullStars();
-    const halfStars = this.halfStars();
     const emptyStars = this.emptyStars();
 
     const renderFullStars = () => (fullStars !== 0
       ? Array(fullStars)
         .fill(null)
         .map((item, i) => <FontAwesomeIcon className="star" key={`fs${i}`} icon="star" />)
-      : '');
-
-    const renderHalfStars = () => (halfStars !== 0
-      ? Array(halfStars)
-        .fill(null)
-        .map((item, i) => (
-          <span key={`hs${i}`} className="fa-layers fa-fw star">
-            <FontAwesomeIcon icon="star-half" />
-            <FontAwesomeIcon icon={['far', 'star-half']} flip="horizontal" />
-          </span>
-        ))
       : '');
 
     const renderEmptyStars = () => (emptyStars !== 0
@@ -62,7 +43,6 @@ export default class StarRating extends Component {
     return (
       <div className="star-rating">
         {renderFullStars()}
-        {renderHalfStars()}
         {renderEmptyStars()}
       </div>
     );
