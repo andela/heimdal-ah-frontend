821483071232c16628ea7e3cb52bda8b44e05117
Henry <pienry3g@gmail.com>
2019-01-15 10:50:28 +0100

    [Feature #163172710] Add Glow Test

---
+++ src/component/UI/buttons/glow/Glow.spec.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import Glow from './Glow';
+
+describe('<Button />', () => {
+  it('should render the Glow component', () => {
+    const wrapper = shallow(<Glow />);
+    expect(wrapper.length).toBe(1);
+  });
+});
