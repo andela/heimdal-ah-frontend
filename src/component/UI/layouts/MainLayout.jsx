bbb82428d76d80f1c32c910d47beedf0ec576dae
Oluwaseun Jolaoso <oluwaseunjolaoso@Oluwaseuns-MacBook-Pro.local>
2019-01-15 08:46:07 +0100

    [Feature 163155405] write docs for mainlayout component

--- src/component/UI/layouts/MainLayout.jsx
+++ src/component/UI/layouts/MainLayout.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import PropsTypes from 'prop-types';
 import './MainLayout.scss';
 
+/**
+ * @description It wraps its children with a layout style
+ * @param {object} props - react props with children
+ * @returns {object} the component
+ */
 const MainLayout = ({ children }) => (
   <div className="p-5 content">
     {children}
