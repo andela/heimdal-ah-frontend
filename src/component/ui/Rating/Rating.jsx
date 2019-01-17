8f353823cc0c8b29f7f655674295e2b5c5807c92
pitaz <peter.odekwo@gmail.com>
2019-01-17 11:27:02 +0100

    [feature #163166708] resolve directory error in Rating file

--- src/component/ui/Rating/Rating.jsx
+++ src/component/ui/Rating/Rating.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { range } from 'lodash';
-import './Ratings.scss';
+import './Rating.scss';
 
 const Rating = props => (
   <div className="rate">
