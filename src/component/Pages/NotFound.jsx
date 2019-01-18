eb14ee20dafbd0106877712073bc498c09287501
el-joft <ottimothy@gmail.com>
2019-01-18 12:45:23 +0100

    [finishes #163154901] Fixing hound and code climate issues

--- src/component/Pages/NotFound.jsx
+++ src/component/Pages/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => (
     <h3>Not Found 404</h3>
     <div>
       <a to='/'>
-        <div>..go back Home...</div>
+        <div>..go back Home....</div>
       </a>
     </div>
   </div>
