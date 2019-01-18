cd29f38d70f72b5e3db98a89d3523f6037356c13
el-joft <ottimothy@gmail.com>
2019-01-18 13:16:43 +0100

    [finishes #163154901] Create Header component

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../ui/header/Header.jsx';
-/**
- * Layout component
- */
+
 class Layout extends Component {
   state = {
     isAuthenticated: false,
