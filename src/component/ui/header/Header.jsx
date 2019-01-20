317cd4926cf3119fa2c12793d713aa154354fc33
el-joft <ottimothy@gmail.com>
2019-01-20 16:13:06 +0100

    [finishes #163154901] Create Header component

--- src/component/ui/header/Header.jsx
+++ src/component/ui/header/Header.jsx
@@ -20,7 +20,7 @@ const Header = (props) => {
           <ul className="navbar-nav mr-auto">
             <NavItems />
           </ul>
-          {!isValidated ? <ActiveUser /> : <HeaderButton />}
+          {isValidated ? <ActiveUser /> : <HeaderButton />}
           <SearchForm />
         </div>
       </nav>
