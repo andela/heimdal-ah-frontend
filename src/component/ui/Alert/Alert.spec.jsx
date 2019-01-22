1fa8da4a8e94bab61aba10cee1dc81313e6df2da
Patrick Azu <azupatrick0@gmail.com>
2019-01-22 22:20:36 +0100

    [Feature #162170980]: fix failing test

--- src/component/ui/Alert/Alert.spec.jsx
+++ src/component/ui/Alert/Alert.spec.jsx
@@ -8,12 +8,6 @@ describe('Password Reset Component Test Suite', () => {
       const wrapper = shallow(<Alert type='success' title='Alert' message='This is alert' />);
       expect(wrapper.length).toBe(1);
       expect(wrapper).toMatchSnapshot();
-      expect(wrapper.instance().props.type).to.be.a('string');
-      expect(wrapper.instance().props.title).to.be.a('string');
-      expect(wrapper.instance().props.message).to.be.a('string');
-      expect(wrapper.instance().props.type.length).to.be.gt(0);
-      expect(wrapper.instance().props.title.length).to.be.gt(0);
-      expect(wrapper.instance().props.message.length).to.be.gt(0);
     });
   });
 });
