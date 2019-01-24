16eb9fbbb8f558b762da1bbec25dfec413a993fa
pitaz <peter.odekwo@gmail.com>
2019-01-24 14:16:51 +0100

    [feature #163294609] add class documentation

--- src/component/ui/Modal/TriggerModal.jsx
+++ src/component/ui/Modal/TriggerModal.jsx
@@ -5,18 +5,22 @@ import Button from '../Buttons/Button';
 
 /**
  * @description - Heimdal Authors TriggerModal Component
- * @param {props} Class - the class of the button(bootstrap or custom)
+ * @param {props} type - the class of the button(bootstrap or custom)
+ * @param {props} label - the label of the modal button
+ * @param {props} className - the class of the modal
+ * @param {props} children - the children components to be passed
  * @param {function} toggle - the function to be performed when the modal is triggered
  * @returns {fragment} Modal fragment
  */
+
 class TriggerModal extends Component {
   state = {
-    modal: false,
+    display: false,
   };
 
   toggle = () => {
     this.setState(prevState => ({
-      modal: !prevState.modal,
+      display: !prevState.display,
     }));
   };
 
@@ -30,7 +34,7 @@ class TriggerModal extends Component {
           onClick={this.toggle}
         />
         <Modal
-          isOpen={this.state.modal}
+          isOpen={this.state.display}
           toggle={this.toggle}
           className={this.props.className}
         >
