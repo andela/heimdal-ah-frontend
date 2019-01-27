2e9cf69bde2110087b07f009a38806943f9210e9
Sunny Edogbo <sunny.edogbo@andela.com>
2019-01-27 14:37:00 +0100

    [feature 162170991] write test for reducers

--- src/config/http.js
+++ src/config/http.js
@@ -4,7 +4,6 @@ const instance = axios.create({
   baseURL: `${process.env.BASE_URL_PROD}/api/v1`,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJuYW1lIjoid2FsZSIsInJvbGVJZCI6MSwiaWF0IjoxNTQ4MzczNjExLCJleHAiOjE1NDg0NjAwMTF9.eWuLzQcwOE0M6L4a5Irnjp6oV-MAHzjC6Ka8Aj5A218',
   },
 });
 
