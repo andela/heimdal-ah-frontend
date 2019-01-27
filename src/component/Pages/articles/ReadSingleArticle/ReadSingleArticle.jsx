3c5877a5bdbfa3f1208983103e4182b6970641f7
victor obije <konigeobije@gmail.com>
2019-01-27 18:07:47 +0100

    [feature 162170984] update on Read a single article

---
+++ src/component/Pages/articles/ReadSingleArticle/ReadSingleArticle.jsx
@@ -0,0 +1,90 @@
+import React, { Fragment } from 'react';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+import './ReadSingleArticle.scss';
+import Rating from '../../../ui/Rating/Rating';
+import Glow from '../../../ui/Buttons/glow/Glow';
+
+
+// /**
+//  * @description - CreateArticle Class
+//  */
+// class ReadSingleArticle extends React.Component {
+//   /**
+//  * @description - render the editors
+//  * @param {props} status - the status returned from dispatching actions
+//  * @returns {component} Component
+//  */
+//   render() {
+const ReadSingleArticle = () => (
+  <Fragment>
+    <div className='article-header'>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Fugit, totam!
+      </h1>
+    </div>
+    <div className='bookmark-row'>
+      <div />
+      <div>
+        <img className='bookmark-logo' src='/src/images/bookmark.svg' alt='bookmark-logo' />
+      </div>
+    </div>
+    <div className='body'>
+      <div className='article-section'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quibusdam. Minima, laboriosam sed? Ex labore
+                  voluptatibus fugiat sit dicta obcaecati deleniti esse illum architecto? Incidunt,
+                  ipsam? Nihil facere aliquam minima nostrum eum! Perferendis adipisci temporibus, ipsam molestiae
+                  inventore mollitia fuga! Rem, vitae. Fuga ab harum dolores ea error tenetur. Autem vel esse fuga.
+                  Nisi sit atque aperiam reiciendis
+                  labore dolorum id, quo praesentium mollitia beatae voluptatum aliquam dolor, ut quibusdam
+                    iure fugit dicta tenetur et aliquid eos neque ducimus voluptas!
+      </div>
+      <div className='user-section'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quibusdam. Minima,
+                  laboriosam sed? Ex labore
+                  voluptatibus fugiat sit dicta obcaecati deleniti esse illum architecto? Incidunt,
+                  ipsam? Nihil facere aliquam minima nostrum eum! Perferendis adipisci
+                  temporibus, ipsam molestiae inventore mollitia fuga! Rem, vitae. Fuga ab harum dolores ea error tenetur. Autem vel esse fuga.
+                  Nisi sit atque aperiam reiciendis labore dolorum id, quo praesentium mollitia beatae voluptatum aliquam dolor, ut quibusdam
+                    iure fugit dicta tenetur et aliquid eos neque ducimus voluptas!
+      </div>
+      <div className='bottom-details'>
+        <div>
+          <Rating />
+        </div>
+        <div className='ratings'>
+          <span>Rated by:</span>
+        </div>
+        <div className='glow'>
+          <Glow />
+        </div>
+      </div>
+      <div />
+      <div className='line' />
+    </div>
+    <div className='tag-buttons'>
+      <button> hello </button>
+      <button> hello </button>
+      <button> hello </button>
+    </div>
+  </Fragment>
+);
+//   }
+// }
+
+// const mapStateToprops = state => ({
+
+// });
+
+// const matchDispatchToProps = dispatch => ({
+//   actions: bindActionCreators(
+//     {
+//       createArticleAction,
+//     },
+//     dispatch,
+//   ),
+// });
+
+
+export default ReadSingleArticle;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,6 +5,7 @@ import LoginPage from '../component/Pages/LoginPage.jsx';
 import NotFound from '../component/Pages/NotFound.jsx';
 import MainLayout from '../component/Layout/MainLayout/MainLayout';
 import CreateArticle from '../component/Pages/articles/createArticle/CreateArticles';
+import ReadSingleArticle from '../component/pages/articles/ReadSingleArticle/ReadSingleArticle';
 import HomePage from '../component/Pages/HomePage';
 import PasswordResetPresentation from '../component/Pages/PasswordResetPage/PasswordResetPresentation';
 import UpdatePasswordPresentation from '../component/Pages/PasswordResetPage/UpdatePasswordPresentation';
@@ -16,6 +17,7 @@ const Routes = () => (
       <Route exact path='/' component={HomePage} />
       <Route exact path='/login' component={LoginPage} />
       <Route exact path='/article/create' component={CreateArticle} />
+      <Route exact path='/article/single' component={ReadSingleArticle} />
       <Route exact path='/updatearticle' component={UpdateArticles} />
       <Route exact path='/resetpassword' component={PasswordResetPresentation} />
       <Route exact path='/updatepassword' component={UpdatePasswordPresentation} />
