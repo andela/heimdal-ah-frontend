2086fdbbe872631ca293eefb6e9b9282e63a62c6
Sunny Edogbo <sunny.edogbo@andela.com>
2019-01-31 11:23:59 +0100

    [feature 163486215]fix merge conflicts

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -7,7 +7,6 @@ import HomePage from '../component/Pages/HomePage.jsx';
 import SocialAuth from '../component/ui/SocialAuth/SocialAuth';
 import PasswordResetPresentation from '../component/Pages/PasswordResetPage/PasswordResetPresentation';
 import UpdatePasswordPresentation from '../component/Pages/PasswordResetPage/UpdatePasswordPresentation';
-import ProfileEdit from '../component/Pages/ProfileEdit/ProfileEdit';
 import Profile from '../component/Pages/Profile/Profile';
 import Articles from '../component/Pages/Article/ArticleListPage';
 import ProtectedRoute from './ProtectedRoute';
@@ -19,7 +18,6 @@ const Routes = () => (
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/social-auth' component={SocialAuth} />
-        <ProtectedRoute exact path='/profile/edit' component={ProfileEdit} />
         <ProtectedRoute exact path='/profile/:username?' component={Profile} />
         <Route exact path='/resetpassword' component={PasswordResetPresentation} />
         <Route exact path='/reset-password' component={PasswordResetPresentation} />
