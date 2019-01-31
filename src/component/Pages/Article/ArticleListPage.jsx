8a704252e958081613977a34543100ef0d021ed7
Sunny Edogbo <sunny.edogbo@andela.com>
2019-01-31 13:46:29 +0100

    [feature 163486215]Edit hover colors

--- src/component/Pages/Article/ArticleListPage.jsx
+++ src/component/Pages/Article/ArticleListPage.jsx
@@ -6,7 +6,7 @@ import ArticleList from '../../../container/Article/Article';
  */
 export default function ArticleListPage() {
   return (
-    <div>
+    <div className='mb-5'>
       <h2 className='mx-auto text-center mb-5'> All Stories</h2>
       <ArticleList />
     </div>
