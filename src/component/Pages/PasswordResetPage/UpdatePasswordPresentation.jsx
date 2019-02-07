aba07723cdfd5674cfb3e180332aabe176053f7d
Patrick Azu <azupatrick0@gmail.com>
2019-02-07 21:43:01 +0100

    [Bug #163797500]: update reset password page

--- src/component/Pages/PasswordResetPage/UpdatePasswordPresentation.jsx
+++ src/component/Pages/PasswordResetPage/UpdatePasswordPresentation.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Redirect } from 'react-router-dom';
-import Button from '../../ui/Buttons/Button';
 import Alert from '../../ui/Alert/Alert';
 import './PasswordReset.scss';
 // eslint-disable-next-line import/no-named-as-default
@@ -11,9 +10,11 @@ const UpdatePasswordPresentation = () => (
     {
         data => (
           <Fragment>
-            {data.status === 'SUCCESS' && <Redirect to='/login' />}
+            {data.status === 'SUCCESS' && <Alert title='Password Update' message='Password updated successfully' type='success' />}
+            {data.status === 'SUCCESS' && <Redirect to='/' />}
             {data.status === 'FAILED' && <Redirect to='/resetpassword' />}
-            <div className='password-reset body'>
+            <br />
+            <div className='password-reset update-box'>
               <p>Enter Your New Password</p>
               {data.status === 'ERROR' && <Alert type='warning' title='Email not sent' message='Server Error' />}
               <form onSubmit={e => data.onHandleSubmit(e)}>
@@ -23,7 +24,7 @@ const UpdatePasswordPresentation = () => (
                 <input type='password' className='password-reset input-reset-password' name='confirmpassword' value={data.confirmpassword} placeholder='Confirm Password' onChange={e => data.onHandleChange(e)} required />
                 <br />
                 <br />
-                <Button type='login2' label='Update' Class='password-reset button' />
+                <button type='submit' className='password-reset submit-button'>Update</button>
               </form>
             </div>
             <br />
